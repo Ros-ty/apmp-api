794894db8aa579c67db74042f76c53d79e5a70e7
Ростислав М'якуш <czarmr1@gmail.com>
2018-08-02 16:52:58 +0300

    cofigure_permitted_parameters change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password) }
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :phone_number, :email, :password, :current_password) }
+    # devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password) }
+    # devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
+    # devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :phone_number, :email, :password, :current_password) }
   end
 end
